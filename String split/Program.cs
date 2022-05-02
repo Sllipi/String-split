574804595dee5c6f89752701c45bc2609bdbc519
Sllipi <Sllipi@SLLIPI->
2022-05-02 23:17:09 +0300

    Добавьте файлы проекта.

---
+++ String split/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace String_split
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сообщение!");
+            string str = Convert.ToString(Console.ReadLine());
+            string[] line = str.Split(' ');
+            foreach (var lineStr in line)
+            {
+                Console.WriteLine(lineStr);
+            }
+        }
+    }
+}
